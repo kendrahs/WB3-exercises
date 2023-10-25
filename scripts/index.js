38621a9f7f1405dd355580f6a41f36dd2f4528d6
kendrah <kentinez11@gmail.com>
2023-10-25 16:14:50 -0400

    still have to fix

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,7 @@
 function buttonConvertClick() {
     const inputDate = document.getElementById ("inputDate").ariaValueMax;
     console.log(inputDate);
+    let dateObj = new Date(inputDate)
 
 
 let date = dateObj.toISOString();
