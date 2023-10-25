07213603963c0089a8f1196a19ca83b2fe4de63d
kendrah <kentinez11@gmail.com>
2023-10-25 14:53:18 -0400

    Date folders and file

---
+++ Dates/DateScripts/date_difference.js
@@ -0,0 +1,4 @@
+"use script";
+
+let startDate = "July 11, 2022";
+let endDate = "November 11, 2022";
--- Functions/demo9.js
+++ Functions/demo9.js
@@ -1,6 +1,11 @@
-let d = new Date();
+//p 57
+let d = new Date(2020, 9, 31); //oct is 9
 console.log(d);
 
 console.log(d.toString());
 
 console.log(d.toUTCString());
+
+console.log(d.toTimeString());
+
+console.log(d.toLocaleString());
